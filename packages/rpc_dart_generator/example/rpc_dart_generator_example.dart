34d1c7365650789dbe4c0eeb6b5b8da685d8089e
nogipx <nogipx@gmail.com>
2025-11-30 23:51:14 +0300

    add rpc dart generator

---
+++ packages/rpc_dart_generator/example/rpc_dart_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:rpc_dart_generator/rpc_dart_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/rpc_dart_generator/test/rpc_dart_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:rpc_dart_generator/rpc_dart_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
