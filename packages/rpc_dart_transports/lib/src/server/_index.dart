6c286dc7f309f92e0e4043626e1de1d55ce26792
nogipx <nogipx@gmail.com>
2026-01-23 22:31:54 +0300

    remove: rpc_dart_transports

--- packages/rpc_dart_transports/lib/src/server/_index.dart
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Karim "nogipx" Mamatkazin <nogipx@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
-export 'rpc_server_interface.dart';
--- packages/rpc_dart_transports/lib/src/transports/http/_index.dart
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Karim "nogipx" Mamatkazin <nogipx@gmail.com>
-//
-// SPDX-License-Identifier: MIT
-
-export 'rpc_http1_caller_transport.dart';
-export 'rpc_http1_responder_transport.dart';
-export 'rpc_http1_server.dart';
